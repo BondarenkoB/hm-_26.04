4df389e6d76d1db52549bc9c6cf568f89eb41ec9
Bohdan <bohdanstep23@gmail.com>
2023-04-29 22:32:21 +0300

    done

--- hm_26.04.py
+++ hm_26.04.py
@@ -1,1 +1,9 @@
+with open('test.txt', 'r') as file:
+    contents = file.readlines()
+
+contents.pop(2) 
+
+with open('test.txt', 'w') as file:
+    contents = ''.join(contents)
+    file.write(contents)
 

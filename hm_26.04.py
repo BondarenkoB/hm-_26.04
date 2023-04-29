df2924851544bd89a6d11d73d272bff0fe67ea29
Bohdan <bohdanstep23@gmail.com>
2023-04-29 22:26:14 +0300

    done

--- hm_26.04.py
+++ hm_26.04.py
@@ -1,1 +1,10 @@
+with open('test.txt', 'r') as file:
+    # Зчитуємо весь вміст файлу
+    file_content = file.read()
 
+# Замінюємо рядок
+new_file_content = file_content.replace('сіль, лавровий лист, зелень – за смаком.', 'salt, bay leaf, herbs - to taste.')
+
+with open('test.txt', 'w') as file:
+    # Записуємо новий вміст у файл
+    file.write(new_file_content)

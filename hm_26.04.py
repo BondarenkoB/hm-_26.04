3b7fc27fdb2232193b6c4eb4b399af064cae35b3
Bohdan <bohdanstep23@gmail.com>
2023-04-29 22:29:27 +0300

    done

--- hm_26.04.py
+++ hm_26.04.py
@@ -1,1 +1,15 @@
+with open('test.txt', 'r') as file:
+    lines = set()
+    duplicates = set()
+    
+    for line in file:
+        if line in lines:
+            duplicates.add(line)
+        else:
+            lines.add(line)
+
+if duplicates:
+    print(f'Файл має дублікати рядків: {duplicates}')
+else:
+    print('Файл не має дублікати рядків')
 
